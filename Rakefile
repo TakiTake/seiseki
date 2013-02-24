eb516d559b939ceb369b3277444a5e56aa02792a
Takizawa Takeshi <takitake.create@gmail.com>
2013-02-24 21:57:19 +0900

    add files

---
+++ Rakefile
@@ -0,0 +1,62 @@
+require 'benchmark'
+require "rubygems"
+require "bundler/setup"
+
+Bundler.require
+
+@root = File.expand_path('..', __FILE__)
+@start = Date.new(2000,  1,  1)
+@end   = Date.new(2000, 12, 31)
+
+require File.join(@root, '/lib/model.rb')
+
+namespace :db do
+  task :migrate do
+    def save(&block)
+      prng = Random.new(1234)
+
+      # Date loop
+      @start.step(@end).each do |date|
+        # Student loop
+        100.times do |n|
+          student_id = '%03d' % n
+          scores = 5.times.map{ prng.rand(0..100) }
+          attrs = [date.to_s, student_id, *scores]
+
+          yield(attrs)
+        end
+      end
+    end
+
+    Normal.create
+    Binary.create
+
+    Benchmark.bm do |x|
+      x.report('normal') do
+        save{ |attrs| Normal.new.save(attrs) }
+      end
+
+      x.report('binary') do
+        save{ |attrs| Binary.new.save(attrs) }
+      end
+    end
+  end
+end
+
+task :benchmark do
+  def select
+    @start.step(@end).each do |date|
+      yield(date.to_s)
+    end
+  end
+
+  Benchmark.bm do |x|
+    x.report('normal') do
+      select{ |date| Normal.find_by_date(date) }
+    end
+
+    x.report('binary') do
+      select{ |date| Binary.find_by_date(date) }
+    end
+  end
+end
---
+++ lib/model.rb
@@ -0,0 +1,92 @@
+class Model
+  @@table_name = 'seiseki'
+
+  class << self
+    def find_by_date(date)
+    end
+  end
+
+  def save(attrs)
+  end
+end
+
+class Normal < Model
+  @@db = SQLite3::Database.new(File.expand_path('../../db/normal.db', __FILE__))
+
+  class << self
+    def create
+      sql = 'DROP TABLE IF EXISTS seiseki'
+      @@db.execute(sql)
+
+      sql = %(
+        CREATE TABLE seiseki (
+          date text,
+          student_id text,
+          subject1 integer,
+          subject2 integer,
+          subject3 integer,
+          subject4 integer,
+          subject5 integer
+        )
+      )
+      @@db.execute(sql)
+
+      sql = "CREATE INDEX seiseki_idx on seiseki(date)"
+      @@db.execute(sql)
+    end
+
+    def find_by_date(date)
+      sql = "SELECT * FROM #{@@table_name} WHERE date = ?"
+      @@db.execute(sql, date)
+    end
+  end
+
+  def save(attrs)
+    sql = "INSERT INTO #{@@table_name} VALUES (?, ?, ?, ?, ?, ?, ?)"
+    rst = []
+    @@db.execute(sql, attrs) do |row|
+      rst << row
+    end
+
+    rst
+  end
+end
+
+class Binary < Model
+  @@db = SQLite3::Database.new(File.expand_path('../../db/binary.db', __FILE__))
+
+  class << self
+    def create
+      sql = 'DROP TABLE IF EXISTS seiseki'
+      @@db.execute(sql)
+
+      sql = %(
+        CREATE TABLE seiseki (
+          date text,
+          student_id text,
+          subject bolb
+        )
+      )
+      @@db.execute(sql)
+
+      sql = "CREATE INDEX seiseki_idx on seiseki(date)"
+      @@db.execute(sql)
+    end
+
+    def find_by_date(date)
+      sql = "SELECT * FROM #{@@table_name} WHERE date = ?"
+      rst = []
+      @@db.execute(sql, date) do |row|
+        rst << [row[0], row[1], row[2].unpack('w5')].flatten
+      end
+
+      rst
+    end
+  end
+
+  def save(attrs)
+    month, student_id, *data = attrs
+    sql = "INSERT INTO #{@@table_name} VALUES (?, ?, ?)"
+    @@db.execute(sql, [month, student_id, data.pack('w5')])
+  end
+end
